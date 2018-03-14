dfddd3d34c72cf685584bc0453d80d57cde5666b
upday <upday7@163.com>
2018-03-14 22:25:17 +0800

    unicode fix

--- TomatoClock/lib/sounds.py
+++ TomatoClock/lib/sounds.py
@@ -4,6 +4,12 @@ _SOUND_DIR = os.path.join(
     os.path.split(os.path.dirname(__file__))[0],
     "sounds"
 )
+
+try:
+    _SOUND_DIR = unicode(_SOUND_DIR)
+except UnicodeError:
+    _SOUND_DIR = unicode(_SOUND_DIR, 'gbk')
+
 BREAK = os.path.join(_SOUND_DIR, "break.mp3")
 ABORT = os.path.join(_SOUND_DIR, "abort.mp3")
 START = os.path.join(_SOUND_DIR, "page.mp3")
